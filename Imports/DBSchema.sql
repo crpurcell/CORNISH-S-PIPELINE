307bdfe2d6c2f5d8bc51089b202274514abdd0b7
crpurcell <cormac.r.purcell@gmail.com>
2017-02-06 10:45:08 +1100

    Modified DB schema.

--- Imports/DBSchema.sql
+++ Imports/DBSchema.sql
@@ -8,6 +8,8 @@ nAnts INTEGER,
 fieldName VARCHAR(50),
 RA_deg DOUBLE,
 Dec_deg DOUBLE,
+goodvis DOUBLE,
+badvis DOUBLE,
 splitFlag INTEGER,
 useFlag INTEGER
 );
@@ -36,3 +38,23 @@ RA_deg DOUBLE,
 Dec_deg DOUBLE,
 fieldName VARCHAR(50));
 
+CREATE TABLE field_images (
+fieldName VARCHAR(50) PRIMARY KEY,
+tileID INTEGER,
+RA_deg DOUBLE,
+Dec_deg DOUBLE,
+no_fields INTEGER,
+pix_max DOUBLE,
+pix_min DOUBLE,
+npix_x INTEGER,
+npix_y INTEGER,
+pixscale_x DOUBLE,
+pixscale_y DOUBLE,
+bm_maj DOUBLE,
+bm_min DOUBLE,
+bm_pa DOUBLE,
+rms_quality DOUBLE,
+rms_mult DOUBLE,
+uvrange_min DOUBLE,
+uvrange_max DOUBLE,
+obs_includedVARCHAR(50));
